ecba3de022f07f83867c86099d64e889fc477d1d
actatum88 <4946@holbertonschool.com>
2022-08-30 20:47:53 -0700

    task 3

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+i = abs(number) % 10
+string = "and is less than 6 and not 0"
+if number < 0:
+    i *= -1
+if i == 0:
+    print("Last digit of {} is {} and is 0" .format(number, i))
+elif i > 5:
+    print("Last digit of {} is {} and is greater than 5" .format(number, i))
+elif i < 6:
+    print("Last digit of {} is {} {}" .format(number, i, string)
