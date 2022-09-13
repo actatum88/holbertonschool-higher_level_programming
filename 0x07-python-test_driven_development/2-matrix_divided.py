e0e88094d6b91d7528128d36c9fa979f2e416321
actatum88 <alexandra.c.tatum@gmail.com>
2022-09-12 23:30:46 -0500

    Editing

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Divides all elements of a matrix
+"""
+
+
+def matrix_divided(matrix, div):
+"""
+Function that divides all elements of a matrix
+"""
+raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+raise TypeError("Each row of the matrix must have the same size")
+raise TypeError("div must be a number")
+raise ZeroDivisionError("division by zero")
