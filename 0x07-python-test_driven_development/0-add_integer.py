13abc98d52d71fc252432264c9f6564adb29d4b4
actatum88 <alexandra.c.tatum@gmail.com>
2022-09-12 20:23:37 -0500

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-"""Returns the sum of two integers, or floats typecasted as int
+"""
+Add two integers and typecast to integer if float
+
 """
 
 
 def add_integer(a, b=98):
-    def add_num(n1,n2):
-        sum = n1 + n2
-    return sum
+    """Returns the sum of two integers a and b,
+       returns an error if a and b are not ints or floats"""
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError('a must be an integer')
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b must be an integer')
+
+    if type(a) is float:
+        a = int
+    if type(b) is float:
+        b = int
+
+    return a + b
