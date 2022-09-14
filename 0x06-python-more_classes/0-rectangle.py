01a69eac26713c9201ae4cc5bbaa06ed2ac1174b
actatum88 <alexandra.c.tatum@gmail.com>
2022-09-14 13:25:00 -0500

    Task 0

---
+++ 0x06-python-more_classes/0-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Defines a rectangle class
+"""
+
+
+class Rectangle:
+    """ Class that defines a rectangle """
+
+    def __init__(self, width=0, height=0):
+        """ Initializes a new rectangle. """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Gets and sets the width of the rectangle """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Gets and sets the height of the rectangle """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
