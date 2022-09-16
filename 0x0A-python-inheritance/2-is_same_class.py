92c224ef18ba15476ee008b5a511234363e7fe98
actatum88 <alexandra.c.tatum@gmail.com>
2022-09-15 22:03:52 -0500

    Task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Shebang! """
+
+
+def is_same_class(obj, a_class):
+    """ Is the object is exactly an instance of the specified class, T or F? """
+    return True if type(obj) is a_class else False
