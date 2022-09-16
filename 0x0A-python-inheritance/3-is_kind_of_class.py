17f6bd2168367dc04f3e93091b52f3a1748720fd
actatum88 <alexandra.c.tatum@gmail.com>
2022-09-15 22:33:13 -0500

    Task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-""" Module 3-is_kind_of_class """
+""" Task 3 is kind of class module """
+
 
 def is_kind_of_class(obj, a_class):
-    """Is the object is exactly an instance of the specified class, T or F? """
+    """
+    Returns True if the object is an instance of, or if the object
+    is an instance of a class that inherited from,
+    the specified class ; otherwise False
+    """
     return isinstance(obj, a_class)
