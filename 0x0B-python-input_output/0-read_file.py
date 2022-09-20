d73150796d93056bd9deacc36383520bcc444f77
actatum88 <alexandra.c.tatum@gmail.com>
2022-09-19 22:07:51 -0500

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Module 0-read_file.py """
+""" 0 Read File """
 
 
 def read_file(filename=""):
-    """ Reads a text file and prints to stdout. utf-8 is standard if encoding is not specific. """
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """ Read a file """
+    with open(filename, encoding="utf=8") as myFile:
+        print(myFile.read(), end="")
