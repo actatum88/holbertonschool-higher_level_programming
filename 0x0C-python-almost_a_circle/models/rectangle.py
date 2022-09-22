9845fec134b292119146f7e45b060ef5bb25a6de
actatum88 <alexandra.c.tatum@gmail.com>
2022-09-22 12:57:23 -0500

    rectangle module

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,38 +1,26 @@
 #!/usr/bin/python3
-""" Module Rectangle """
+"""
+Module Rectangle
+"""
 
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """defining rectangle class"""
+    """
+    defines class Rectangle; inherits from class Base
+    
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """initializing rectangle class"""
+        """Initialize rectangle"""
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
-        if type(x) is not int:
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        if type(y) is not int:
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-@property
+    @property
     def width(self):
         """get width"""
         return self.__width
@@ -54,7 +42,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        """setr width"""
+        """setter width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -101,12 +89,11 @@ class Rectangle(Base):
                         for rows in range(self.__height)))
 
     def __str__(self):
-        """ Returns string representation of Rectangle """
-
-        return (f'[Rectangle] ({self.id}) '
-                f'{self.__x}/{self.__y} - '
-                f'{self.__width}/{self.__height}')
-
+        """Prints [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
+                        self.__class__.__name__, self.id,
+                        self.__x, self.__y,
+                        self.__width, self.__height)
 
     def update(self, *args, **kwargs):
         """
