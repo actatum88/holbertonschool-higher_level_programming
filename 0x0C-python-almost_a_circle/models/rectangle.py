f38f1a94e1b50db72d1cfe880e52d97f67be7a24
actatum88 <alexandra.c.tatum@gmail.com>
2022-09-22 12:50:24 -0500

    rectangle and square

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -32,3 +32,118 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+@property
+    def width(self):
+        """get width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """get x"""
+        return self.__x
+
+    @property
+    def y(self):
+        """get y"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """setr width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """set height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """set x"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """set y"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Area of the rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """ Prints rectangle with #'s """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        print("\n" * self.__y +
+              "\n".join(" " * self.__x + "#" * self.__width
+                        for rows in range(self.__height)))
+
+    def __str__(self):
+        """ Returns string representation of Rectangle """
+
+        return (f'[Rectangle] ({self.id}) '
+                f'{self.__x}/{self.__y} - '
+                f'{self.__width}/{self.__height}')
+
+
+    def update(self, *args, **kwargs):
+        """
+        If args: set attributes in this order: id, width, height, x, y
+        If no args given: set attributes according to kwargs
+        """
+        if args:
+            for key, value in enumerate(args):
+                if key == 0:
+                    self.id = value
+                elif key == 1:
+                    self.width = value
+                elif key == 2:
+                    self.height = value
+                elif key == 3:
+                    self.x = value
+                else:
+                    self.y = value
+        else:
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                if k == "width":
+                    self.width = v
+                if k == "height":
+                    self.height = v
+                if k == "x":
+                    self.x = v
+                if k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        """Return dictionary representation"""
+        dic = {}
+        dic["id"] = self.id
+        dic["width"] = self.width
+        dic["height"] = self.height
+        dic["x"] = self.x
+        dic["y"] = self.y
+        return dic
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,64 @@
+  #!/usr/bin/python3
+""" Square module"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+      def __init__(self, size, x=0, y=0, id=None):
+        """Initialize square"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set size """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Prints square"""
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
+            self.__class__.__name__, self.id, self.x, self.y,
+            self.size)
+
+    def update(self, *args, **kwargs):
+        """
+        If args: set attributes in this order: id, width, height, x, y
+        If no args given: set attributes according to kwargs
+        """
+        if args:
+            for key, value in enumerate(args):
+                if key == 0:
+                    self.id = value
+                elif key == 1:
+                    self.size = value
+                elif key == 2:
+                    self.x = value
+                else:
+                    self.y = value
+        else:
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                if k == "size":
+                    self.size = v
+                if k == "x":
+                    self.x = v
+                if k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        """Return dictionary representation"""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
