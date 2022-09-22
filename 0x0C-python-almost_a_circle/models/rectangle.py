acbe21f4ca51dacab4d223efe7e7cd0719200a54
actatum88 <alexandra.c.tatum@gmail.com>
2022-09-22 10:31:26 -0500

    Task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""module for rectangular model"""
+from re import X
+from turtle import width
+from models.base import Base
+
+
+class Rectangle(Base):
+    """defining rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initializing rectangle class"""
+        super().__init__(id)
+
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
