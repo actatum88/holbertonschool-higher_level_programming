b9684110b6ccd9d3c6fc15e07554234bdc7b76f4
actatum88 <alexandra.c.tatum@gmail.com>
2022-11-01 14:36:31 -0500

    Initial commit

---
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetches status from Holberton Intranet"""
+
+if __name__ == '__main__':
+    import urllib.request
+    
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(str(html)))
+        print('\t- utf8 content: {}'.format(HTMLdecode))
+        HTMLdecode = html.decode('UTF-8')
