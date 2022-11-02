06e374e8d2521316913377c64c37444df6aee640
actatum88 <alexandra.c.tatum@gmail.com>
2022-11-01 22:50:40 -0500

    bananas

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -1,13 +1,14 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 """catches errors from requests"""
 import urllib.request
 from urllib.error import HTTPError
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
+
     try:
-        with urllib.request.urlopen(sys.argv[1]) as resp:
-            print(resp.read().decode('utf-8'))
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
     except HTTPError as e:
         print("Error code: {}".format(e.code))
