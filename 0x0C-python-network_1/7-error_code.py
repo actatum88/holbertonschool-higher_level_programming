6a931cf477a6f074ef3c5eb8b9d3889419fa0fbd
actatum88 <alexandra.c.tatum@gmail.com>
2022-11-01 23:16:51 -0500

    task 7

---
+++ 0x0C-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""catches errors from requests"""
+
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    req = requests.get(url)
+
+
+    print("Error code: {}".format() if req.status_code >= 400 else req.text)
