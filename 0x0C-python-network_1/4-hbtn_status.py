c1d58b1ea7de137310dd89269466c257079f695e
actatum88 <alexandra.c.tatum@gmail.com>
2022-11-04 09:54:44 -0500

    rework task 0 & 4

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
 
     resp = req.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(request.text)))
+    print("\t- content: {}".format(request.text))
