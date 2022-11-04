8d8499e7f41354a18677991a883c2896ddae40e5
actatum88 <alexandra.c.tatum@gmail.com>
2022-11-04 14:22:05 -0500

    Thanks Sarah!

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -4,9 +4,10 @@
 if __name__ == '__main__':
     import urllib.request
     
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print('\t- utf8 content: {}'.format(response.read().decode('utf8')))
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print('\t- utf8 content: {}'.format(page.decode('utf8')))
