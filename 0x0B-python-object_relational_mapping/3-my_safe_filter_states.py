5f361c113703fcf500fd8a78c9d4050b5343271a
Alexandra <alexandra.c.tatum@gmail.com>
2023-02-28 10:01:47 -0800

    Task 3

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""Displays all states from the database hbtn_0e_0_usa where name matches the argument (safe from MySQL injections)"""
 import sys
 import MySQLdb
 
@@ -11,8 +11,10 @@ if __name__ == "__main__":
     # Create cursor to execute SQL queries
     cursor = db.cursor()
 
-    # Execute SQL query to select all states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # Define the SQL query with placeholders and execute it with user input
+    state_name = sys.argv[4]
+    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
 
     # Fetch all rows and print results
     rows = cursor.fetchall()
