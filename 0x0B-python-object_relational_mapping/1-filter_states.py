4b0cf5b9cd30ace78f8013613e7eb050e7fd1167
Alexandra <alexandra.c.tatum@gmail.com>
2023-02-28 09:04:44 -0800

    Initial commit

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    # Create cursor to execute SQL queries
+    cursor = db.cursor()
+
+ # Execute SQL query to select all states starting with N
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Fetch all rows and print results
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connections
+    cursor.close()
+    db.close()
---
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Displays all states from the database hbtn_0e_0_usa where name matches the argument"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], )
+
+    # Create cursor to execute SQL queries
+    cursor = db.cursor()
+
+    # Define the SQL query to select all states where name matches the argument
+    state_name = sys.argv[4]
+    query = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(state_name)
+
+    # Execute the SQL query
+    cursor.execute(query)
+
+    # Fetch all rows and print results
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connections
+    cursor.close()
+    db.close()
