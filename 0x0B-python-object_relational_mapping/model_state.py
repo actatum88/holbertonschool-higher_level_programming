0ec93717f2821be2d7413938a9649a29f14f880c
Alexandra <alexandra.c.tatum@gmail.com>
2023-03-01 08:13:32 -0800

    update

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
-from SQLAlchemy import Column, Integer, String
-from SQLAlchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 """Base Model"""
 
