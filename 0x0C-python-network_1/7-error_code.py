c55de9b4abb241d56ffbb54171b7751975822b30
Alexandra <alexandra.c.tatum@gmail.com>
2023-03-14 16:33:52 -0700

    Pycodestyle

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """catches errors from requests"""
 
-
 if __name__ == '__main__':
     import sys
     import requests
 
     url = sys.argv[1]
     req = requests.get(url)
-
 
     print("Error code: {}".format() if req.status_code >= 400 else req.text)
