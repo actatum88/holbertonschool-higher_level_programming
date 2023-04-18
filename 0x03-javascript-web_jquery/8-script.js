b80c67670c9996ec7c6132d9c0472719b6652d03
actatum88 <alexandra.c.tatum@gmail.com>
2023-04-18 17:15:19 -0500

    Updating files

---
+++ 0x03-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $.getJSON("https://swapi-api.hbtn.io/api/films/?format=json", function(data) {
+      var movies = data.results;
+      $.each(movies, function(index, movie) {
+        $("#list_movies").append("<li>" + movie.title + "</li>");
+      });
+    });
+  });
+  
